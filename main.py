6c504a091b3ed1683dd3b3fb58287322a7212a81
Marty <martyumlas@gmail.com>
2025-08-11 21:04:06 +0800

    add more pages to pdf

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ pdf = FPDF(orientation="P", unit="mm", format='A4')
 df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
-    # adding page
     pdf.add_page()
-
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="L", ln=1) #ln is the linebreak, height is suggested same as size
     pdf.line(10, 21, 200, 21)
 
+    for page in range(row['Pages'] - 1):
+        pdf.add_page()
 
 pdf.output('output.pdf')
